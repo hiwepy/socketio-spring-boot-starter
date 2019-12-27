7df06efab376d905bfe1e72be7143a9d85112cb8
wandl <hnxyhcwdl1003@163.com>
2019-12-27 16:30:31 +0800

    修改版权信息

--- src/main/java/com/corundumstudio/socketio/spring/boot/listener/JWTAuthorizationListener.java
+++ src/main/java/com/corundumstudio/socketio/spring/boot/listener/JWTAuthorizationListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, vindell (https://github.com/vindell).
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -22,7 +22,7 @@ import com.corundumstudio.socketio.HandshakeData;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/vindell">wandl</a>
+ * @author 		： <a href="https://github.com/hiwepy">wandl</a>
  */
 public class JWTAuthorizationListener implements AuthorizationListener {
 
